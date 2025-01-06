9b33f7eb9885fae20680f80a89652682f1ba2b96
Anastasiia Martynkova <nastyamartynkova@gmail.com>
2025-01-06 19:44:33 +0100

    finished

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ from skimage.segmentation import active_contour
 from skimage.draw import polygon
 from scipy.ndimage import label
 
-#miles-research-iris-dataset\C-24-125-2-L.jpg
-#miles-research-iris-dataset\D-34-107-4-L.jpg
-#miles-research-iris-dataset\F-14-084-2-L.jpg
-#miles-research-iris-dataset\G-01-100-4-R.jpg
-#miles-research-iris-dataset\G-03-064-1-R.jpg
-#miles-research-iris-dataset\I-27-058-2-L.jpg
-#miles-research-iris-dataset\J-21-064-2-L.jpg
-#miles-research-iris-dataset\K-01-043-1-L.jpg
-#miles-research-iris-dataset\K-10-101-2-L.jpg
-#miles-research-iris-dataset\X-23-165-3-R.jpg
 
 def pupil(image, radius=180):
     # Sharpen the edges
@@ -28,7 +18,7 @@ def pupil(image, radius=180):
 
     # Adjusted circle size (center and radius for a larger circle)
     rows, cols = image.shape
-    center_x, center_y = (cols // 2) - 50, rows // 2
+    center_x, center_y = (cols // 2) - 45, rows // 2
     radius = radius  # Adjust radius to better match the pupil size
 
     # Initialize the contour
@@ -64,7 +54,7 @@ def get_iris_mask(image, radius=500):
 
     # Initialize the contour
     rows, cols = image.shape
-    center_x, center_y = (cols // 2) - 50, rows // 2
+    center_x, center_y = (cols // 2) - 45, rows // 2
     s = np.linspace(0, 2 * np.pi, 400)
     r = center_y + radius * np.sin(s)
     c = center_x + radius * np.cos(s)
@@ -101,7 +91,8 @@ def calculate_eye_freckle_area(image, iris_mask):
     cleaned_freckles = clean_result(thresholded)
 
     freckles_area = np.sum(cleaned_freckles == 255)
-    print(f"Total area of white freckles: {freckles_area} pixels")
+    print("#################################")
+    print(f"Total area of freckles: {freckles_area} pixels")
     freckles_img = image.copy()
     freckles_img[cleaned_freckles == 0] = [0, 0, 0]
 
@@ -111,17 +102,9 @@ def calculate_eye_freckle_area(image, iris_mask):
     # Calculate percentage
     freckles_percentage = (freckles_area / iris_area) * 100
     print(f"Freckles cover {freckles_percentage:.2f}% of the iris.")
-
-    # Display the result
-    fig, ax = plt.subplots(1, 2, figsize=(16, 7))
-    ax[0].imshow(thresholded)
-    ax[0].set_title('Thresholded iris', fontsize=16)
-    ax[0].axis('off')
-    ax[1].imshow(freckles_img)
-    ax[1].set_title('Freckles', fontsize=16)
-    ax[1].axis('off')
-    plt.tight_layout()
-    plt.show()
+    print("#################################")
+    
+    return freckles_img, thresholded
 def remove_noise(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -140,17 +123,7 @@ def remove_noise(image):
     # Inpaint the image using the mask
     inpainted_image = cv2.inpaint(image, dilated, 15, cv2.INPAINT_TELEA)    
 
-    # Display the result
-    fig, ax = plt.subplots(1, 2, figsize=(16, 7))
-    ax[0].imshow(mask)
-    ax[0].set_title('Mask', fontsize=16)
-    ax[0].axis('off')
-    ax[1].imshow(inpainted_image)
-    ax[1].set_title('Iris Segmentation', fontsize=16)
-    ax[1].axis('off')
-    plt.tight_layout()
-    plt.show()
-    return inpainted_image
+    return inpainted_image, mask
 def clean_result(thresholded_image):
     # Remove falsely identified freckles
     dilation_kernel = np.ones((6, 6), np.uint8)
@@ -164,37 +137,54 @@ def clean_result(thresholded_image):
     return dilated_image
 
 
-paths = ["miles-research-iris-dataset\C-24-125-2-L.jpg","miles-research-iris-dataset\G-01-100-4-R.jpg", "miles-research-iris-dataset\G-03-064-1-R.jpg"]  # Add other paths as needed
+paths = [
+    "miles-research-iris-dataset\G-01-100-4-R.jpg",
+    "miles-research-iris-dataset\G-03-064-1-R.jpg",
+    "miles-research-iris-dataset\I-27-058-2-L.jpg",
+    "miles-research-iris-dataset\K-01-043-1-L.jpg",
+    "miles-research-iris-dataset\K-10-101-2-L.jpg",
+    "miles-research-iris-dataset\X-23-165-3-R.jpg"]
 for path in paths:
     # Load the image
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB for correct color display
 
     # Get rid of the light artifacts
-    noiseless_image = remove_noise(img)
+    noiseless_image, noise_mask = remove_noise(img)
     # Sharpen the image
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
     sharp_image = cv2.filter2D(noiseless_image, -1, kernel)
-
     img_gray = (rgb2gray(sharp_image) * 255).astype(np.uint8)  # Convert to grayscale and uint8
 
     # Get iris only image
     iris_mask = get_iris_mask(img_gray)
     roi = noiseless_image * np.stack([iris_mask] * 3, axis=-1)
 
-    # Display the result
-    # fig, ax = plt.subplots(1, 2, figsize=(16, 7))
-    # ax[0].imshow(img_gray, cmap='gray')
-    # ax[0].set_title('Original Image', fontsize=16)
-    # ax[0].axis('off')
-    # ax[1].imshow(roi)
-    # ax[1].set_title('Iris Segmentation', fontsize=16)
-    # ax[1].axis('off')
-    # plt.tight_layout()
-    # plt.show()
-
     # Segment freckles and calculate percentual area
-    calculate_eye_freckle_area(roi, iris_mask)
+    freckles, threshold = calculate_eye_freckle_area(roi, iris_mask)
+
+    # Display the result
+    fig, ax = plt.subplots(2, 3, figsize=(18, 6))
+    ax[0,0].imshow(img)
+    ax[0,0].set_title('Original Image', fontsize=16)
+    ax[0,0].axis('off')
+    ax[0,1].imshow(noise_mask, cmap='grey')
+    ax[0,1].set_title('Noise mask', fontsize=16)
+    ax[0,1].axis('off')
+    ax[0,2].imshow(noiseless_image)
+    ax[0,2].set_title('Image without reflections', fontsize=16)
+    ax[0,2].axis('off')
+    ax[1,0].imshow(roi)
+    ax[1,0].set_title('Segmentation of the iris', fontsize=16)
+    ax[1,0].axis('off')
+    ax[1,1].imshow(threshold, cmap='grey')
+    ax[1,1].set_title('Segmentation of iris with threshold', fontsize=16)
+    ax[1,1].axis('off')
+    ax[1,2].imshow(freckles)
+    ax[1,2].set_title('Freckles', fontsize=16)
+    ax[1,2].axis('off')
+    plt.tight_layout()
+    plt.show()
 
 
 
